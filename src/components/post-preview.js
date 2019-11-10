b81c752026c55855958aa220a40b04b30a0e388e
Keshavaa-S <32556459+Keshavaa-S@users.noreply.github.com>
2019-11-10 12:20:50 -0500

    Added css to blog posts, stiil can't fix graphql error on netify

--- src/components/post-preview.js
+++ src/components/post-preview.js
@@ -3,17 +3,26 @@ import { Link } from 'gatsby'
 import blogStyles from '../pages/blog.module.scss'
 
 const PostPreview = ({ hit }) => {
-    console.log(hit)
     const title = hit.fields.title['en-US']
     const slug = hit.fields.slug['en-US']
     const name = hit.fields.name['en-US']
     const country = hit.fields.country['en-US']
+    const prof = hit.fields.occupation['en-US']
     return (
         <div className={blogStyles.post}>
             <Link to={`/blog/${slug}`}>
             <h2>{title}</h2>
-            <p className={blogStyles.preview}>Story provided by: {name}</p>
-            <p>Country: {country}</p>
+            <div className={blogStyles.tagContainer}>
+                <div className={blogStyles.tagName}>
+                    <h3><span className={blogStyles.tagHeader}>Name:</span> {name}</h3>
+                </div>
+                <div className={blogStyles.tagCountry}>
+                    <h3><span className={blogStyles.tagHeader}>Country:</span> {country}</h3>
+                </div>
+                <div className={blogStyles.tagProf}>
+                    <h3><span className={blogStyles.tagHeader}>Occupation:</span> {prof}</h3>
+                </div>
+            </div>
             </Link>
         </div>
     )
