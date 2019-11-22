209b3a3059f2204afb0284565f0251febf2cb600
Keshavaa-S <32556459+Keshavaa-S@users.noreply.github.com>
2019-11-22 12:23:30 -0500

    New Footer in the works!

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 
 import footerStyles from './footer.module.scss'
-
+import { SocialIcon } from 'react-social-icons';
 import instagram from '../Icons/instagram.png'
-import facebook from '../Icons/facebook.png'
+import facebook from '../Icons/facebook.svg'
 import twitter from '../Icons/twitter.png'
 import email from '../Icons/email.png'
 import linkedin from '../Icons/linkedin.png'
@@ -21,47 +21,39 @@ const Footer = () => {
     `)
 
     return (
+        
         <footer className={footerStyles.footer}>
-            <div className={footerStyles.header}>
-                <div className={footerStyles.title}>
-                    <h1>Pear Impact Project</h1>
-                </div>
-                <div className={footerStyles.icons}>
-                    <img src={instagram}></img>
-                    <img src={facebook}></img>
-                    <img src={twitter}></img>
-                    <img src={linkedin}></img>
-                    <img src={email}></img>
-                </div>
+            <div className={footerStyles.title}>
+                <h1>Migrant Moments</h1>
+                <h2>Stories collected by Azhar Laher</h2>
+                <h2>Toronto, ON</h2>
+                <ul className={footerStyles.icons}>
+                    <li><SocialIcon bgColor="black" fgColor="white" url="http://twitter.com" /></li>
+                    <li><SocialIcon bgColor="black" fgColor="white" url="https://www.facebook.com" /></li>
+                    <li><SocialIcon bgColor="black" fgColor="white" url="https://www.instagram.com" /></li>
+                    <li><SocialIcon bgColor="black" fgColor="white" url="https://www.linkedin.com" /></li>
+                </ul>
+            </div>
+            <div className={footerStyles.pages}>
+                <h1>Pages</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Stories</li>
+                    <li>Contact</li>
+                </ul>
             </div>
 
-            <div className={footerStyles.content}>
-                <div className={footerStyles.pages}>
-                    <ul>
-                        <p>The Project</p>
-                        <li>About</li>
-                        <li>Blog</li>
-                        <li>Contact</li>
-                        <li>Azhar's Website</li>
-                    </ul>
-                </div>
-
-                <div className={footerStyles.info}>
-                    <ul>
-                        <p>More Information</p>
-                        <li>Link 1</li>
-                        <li>Link 2</li>
-                        <li>Link 3</li>
-                        <li>Link 4</li>
-                    </ul>
-                </div>
-
-                <div className={footerStyles.subscribe}>
-                    <h2>Subscribe To Our Newsletter</h2>
-                    <h3>Receive updates whenever Azhar uploads a new story!</h3>
-                    <input placeholder="Enter your email"></input>
-                </div>
+            <div className={footerStyles.pages}>
+                <h1>More</h1>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                    <li>Link 4</li>
+                </ul>
             </div>
+            
         </footer>
     )
 }
