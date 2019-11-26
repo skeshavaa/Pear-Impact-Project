fb9c833bfc4cd50ac6cdc2bfd061f735cdd9a078
Keshavaa-S <32556459+Keshavaa-S@users.noreply.github.com>
2019-11-26 10:46:53 -0500

    Added Contact me form, need to connect to netlify still

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,31 @@ import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
 
+import contactStyles from '../pages/contact.module.scss'
+
 const ContactPage = () => {
     return (
         <div>
             <Head title="Contact"/>
             <Layout>
-                <h1>Contact</h1>
-                <p>The best way to reach me is through email</p>
+                <div className={contactStyles.title}>
+                    <h1>Feel Free To Contact Me!</h1>
+                    <p>I'd love to hear your questions, thoughts, criticisms and more!</p>
+                </div>
+                <div className={contactStyles.wrapper}>
+                   <div className={contactStyles.contactForm}>
+                       <div className={contactStyles.inputFields}>
+                           <input type="text" className={contactStyles.input} placeholder="Name"/>
+                           <input type="text" className={contactStyles.input} placeholder="Email Address"/>
+                           <input type="text" className={contactStyles.input} placeholder="Phone"/>
+                           <input type="text" className={contactStyles.input} placeholder="Subject"/>
+                       </div>
+                       <div className={contactStyles.msg}>
+                           <textarea placeholder="Message"></textarea>
+                           <div className={contactStyles.btn}>Send</div>
+                       </div>
+                   </div>
+                </div>
             </Layout>
         </div>
     )
