b29a89628fd4a115a6d14dd14939a5a13e4af187
skeshavaa <s.keshavaa@gmail.com>
2019-12-03 18:31:35 -0500

    Trying contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -38,6 +38,32 @@ const ContactPage = () => {
                        </div>
                    </div>
                 </div>
+
+
+                <form
+    name="contactsd"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <input type="hidden" name="bot-field" />
+    <div>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </div>
+    <div>
+      <label htmlFor="email">Email</label>
+      <input type="text" name="email" id="email" />
+    </div>
+    <div>
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message" rows="6" required />
+    </div>
+    <div>
+      <input type="submit" value="Drop a line" />
+      <input type="reset" value="Eraser" />
+    </div>
+  </form>
             </Layout>
         </div>
     )
