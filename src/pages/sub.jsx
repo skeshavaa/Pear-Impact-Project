c362d863efd3c0bdc4436a2659e94d8f26c71585
skeshavaa <s.keshavaa@gmail.com>
2020-01-03 19:39:55 -0500

    removed emaillistform

--- src/pages/sub.jsx
+++ src/pages/sub.jsx
@@ -6,7 +6,7 @@ import Head from '../components/head.jsx'
 import contactStyles from '../pages/contact.module.scss'
 import subStyles from '../pages/sub.module.scss'
 
-import EmailListForm from '../components/emailListForm.jsx'
+import EmailListForm from '../components/email-list-form.jsx/index.js'
 
 
 const Sub = () => {
