f0ab75cf9c212d0846a87a8c701b2696e339578e
skeshavaa <s.keshavaa@gmail.com>
2020-05-02 14:45:00 -0400

    anything before pulling

--- src/components/exports.js
+++ src/components/exports.js
@@ -7,6 +7,7 @@ export { default as PostPreview } from "./post-preview";
 export { default as Sidebar } from "./sidebar";
 export { default as Toggle } from "./toggle";
 export { default as Email } from './email'
+export { default as MapChart } from './mapchart';
 /*
 export * from './EmailListForm'
 export * from './filters'
