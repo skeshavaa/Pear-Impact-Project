1abf0d54802c1bf14042a31059d102d09fa4b8e6
skeshavaa <s.keshavaa@gmail.com>
2020-08-24 14:12:52 -0400

    testing css

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,11 +37,6 @@ const IndexPage = () => {
         , integer: true
     })
 
-    var index = null;
-    Axios.get("https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new")
-        .then((resp) => {
-            console.log(resp);
-        })
     const newPosts = useStaticQuery(graphql`
     query {
         allContentfulBlogPost (
