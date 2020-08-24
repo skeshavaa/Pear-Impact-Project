e6a895bbd3932be675c52d4c780601355be98e43
skeshavaa <s.keshavaa@gmail.com>
2020-08-24 15:22:20 -0400

    fixed import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ import { Head, Layout, MapChart } from '@components'
 //Styles
 import homeStyles from '@pageStyles/home.module.scss'
 import postStyles from '@compStyles/post-preview.module.scss'
-import aboutStyles from '@compStyles/about.module.scss'
+import aboutStyles from '@pageStyles/about.module.scss'
 //Pictures
 import mappic from '@images/mapPicture.png'
 import Axios from 'axios'
